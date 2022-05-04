9e9f72baece1ac4c0b5aa7bd487a098f054586ed
Anton Buldenkov <anton@buldenkov.ru>
2022-05-04 17:30:08 +0300

    add cors

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,22 @@
+const allowedCors = [
+  'https://nomoredomains.xyz',
+  'http://nomoredomains.xyz',
+  'localhost:3000'
+];
+
+module.exports.cors = (req, res, next) => {
+  const { origin } = req.headers
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+}
