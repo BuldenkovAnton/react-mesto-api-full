12dae0c0c65a1f91d4c4d0f6b3d800c5c8120b00
Anton Buldenkov <anton@buldenkov.ru>
2022-05-06 09:38:38 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,17 +1,18 @@
 const allowedCors = [
   'https://nomoredomains.xyz',
   'http://nomoredomains.xyz',
-  'localhost:3000'
+  'localhost:3000',
 ];
 
 module.exports.cors = (req, res, next) => {
-  const { origin } = req.headers
+  const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
@@ -19,4 +20,4 @@ module.exports.cors = (req, res, next) => {
   }
 
   return next();
-}
+};
